c6616ca128163f9d6bc17d66644e29c048841e8f
Bameejay <johnsonkayode@gmail.com>
2022-06-24 22:56:02 +0100

    print infos on screen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // Program to prints out your name, height, and country of origin.
 const myName = 'Johnson Francis'
-const myHight = '5.9'
+const myHeight = '5.9'
 const myCountryofOrigin = 'Nigeria'
 
 // document.write(`My name is ${myName} and I am ${myHight} meters tall. I am from ${myCountryofOrigin}`)
-alert(`My name is ${myName} and I am ${myHight} meters tall. I am from ${myCountryofOrigin}`)
+alert(`My name is ${myName} and I am ${myHeight} meters tall. I am from ${myCountryofOrigin}`)
 
-console.log(`My name is ${myName} and I am ${myHight} meters tall. I am from ${myCountryofOrigin}`)
+console.log(`My name is ${myName} and I am ${myHeight} meters tall. I am from ${myCountryofOrigin}`)
+
+document.write(`My name is ${myName} and I am ${myHeight} meters tall. I am from ${myCountryofOrigin}`)
